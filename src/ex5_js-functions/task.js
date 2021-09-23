87cacb65b6cee9cb3d7c17e39ee6fd016133da40
DESKTOP-QMM0050\Михаил <smsrzn@mail.ru>
2021-09-23 15:36:26 +0300

    add fix

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,38 +1,44 @@
 function Calculator() {
   let currentResult = 0;
-  function add(value) {
-    if (typeof value === 'number') {
-      currentResult += value;
-    }
+
+  function add(value = 0) {
+    currentResult += value;
     return add;
   }
-  function subtract(value) {
-    if (typeof value === 'number') {
-      currentResult -= value;
-    }
+
+  function subtract(value = 0) {
+    currentResult -= value;
     return subtract;
   }
-  function divide(value) {
-    if (typeof value === 'number') {
-      currentResult /= value;
-    }
+
+  function divide(value = 1) {
+    currentResult /= value;
     return divide;
   }
-  function multiply(value) {
-    if (typeof value === 'number') {
-      currentResult *= value;
-    }
+
+  function multiply(value = 1) {
+    currentResult *= value;
     return multiply;
   }
+
   function getResult() {
     return currentResult;
   }
+
   function reset() {
     currentResult = 0;
   }
+
   return {
-    add, subtract, divide, multiply, getResult, reset,
+    add,
+    subtract,
+    divide,
+    multiply,
+    getResult,
+    reset,
   };
 }
+
 const calculator = new Calculator();
+
 module.exports = calculator;
